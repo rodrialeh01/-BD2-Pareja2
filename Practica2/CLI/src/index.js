063f20c3c78af53a21e373a62413a00fcb492052
Rodri Hernández <2823897250101@ingenieria.usac.edu.gt>
2023-12-15 14:08:13 -0600

    Agregando la dependencia para crear el CL

--- Practica2/CLI/src/index.js
+++ Practica2/CLI/src/index.js
@@ -1,1 +1,16 @@
-console.log("Aqui va el codigo de la CLI")
+import inquirer from 'inquirer';
+import 'readline-sync';
+inquirer.prompt([
+    {
+      type: 'list',
+      name: 'opcion',
+      message: 'Selecciona una opción:',
+      choices: [
+        { name: 'Opción 1', value: 'opcion1' },
+        { name: 'Opción 2', value: 'opcion2' },
+      ],
+    },
+  ])  
+.then(answers => {
+    console.log('Answer:', answers)
+})
