3d3bf4ee09f27a3c0d7f66104cd149068ee9f237
Andrea Cabrera <3774322450101@ingenieria.usac.edu.gt>
2023-12-19 09:37:11 -0600

    Arreglo de prints en consola

--- Practica2/CLI/src/respaldos/RestaurarRespaldo.js
+++ Practica2/CLI/src/respaldos/RestaurarRespaldo.js
@@ -4,8 +4,8 @@ import { MenuHospital } from '../menu_hospital/menuPrincipal.js';
 import { db_users, db_root } from '../db.js';
 import { exec } from 'child_process';
 
-export const RestaurarRespaldo = ({usuario}, {password}) => {
-    console.log(`${color(66,135,245)}------------------RESTAURAR BACKUP------------------\x1b[0m`);
+export const RestaurarRespaldo = ({ usuario }, { password }) => {
+    console.log(`${color(66, 135, 245)}------------------RESTAURAR BACKUP------------------\x1b[0m`);
     inquirer.prompt([
         {
             type: 'list',
@@ -20,28 +20,29 @@ export const RestaurarRespaldo = ({usuario}, {password}) => {
                     name: 'NO',
                     value: false
                 },
-                
+
             ]
         }
     ]).then(async (answers) => {
-        inquirer.prompt({
-            type: 'input',
-            name: 'nombreBackup',
-            message: `${color(37, 230, 78)}INGRESE EL NOMBRE DEL BACKUP QUE DESEA RESTAURAR (ej. backup1.sql): \x1b[0m`,
-        }).then(async (answers2) => {
-            if (answers.op_menu == true) {
+        if (answers.op_menu == true) {
+            inquirer.prompt({
+                type: 'input',
+                name: 'nombreBackup',
+                message: `${color(37, 230, 78)}INGRESE EL NOMBRE DEL BACKUP QUE DESEA RESTAURAR (ej. backup1.sql): \x1b[0m`,
+            }).then(async (answers2) => {
+
+
                 const dataUser = db_users(usuario, password);
                 const connection = await dataUser.getConnection();
-                
+
                 await connection.query(`USE ${process.env.DB_NAME}`);
                 let nombreBackup = answers2.nombreBackup;
-    
+
                 let query = `mysql -u ${usuario} -p${password} ${process.env.DB_NAME} < ${nombreBackup}`;
                 await connection.query(`INSERT INTO bitacora (nombreUsuario, accion, fechaHoraAccion) VALUES ('${usuario}', 'Restauración de Backup', NOW())`);
-    
-                 exec(query, async (err, stdout, stderr) =>  {
+
+                exec(query, async (err, stdout, stderr) => {
                     if (err) {
-                        console.log(err);
                         console.log(`${color(255, 0, 0)}[ERROR INTERNO] - Se reiniciará la aplicación \x1b[0m`);
                         console.log(`${color(255, 0, 0)}BACKUP NO RESTAURADO\x1b[0m`);
 
@@ -52,14 +53,16 @@ export const RestaurarRespaldo = ({usuario}, {password}) => {
                     }
                     else {
                         console.log(`${color(37, 230, 78)}BACKUP RESTAURADO EXITOSAMENTE\x1b[0m`);
-                        
+
                         await connection.release();
-                        MenuHospital({usuario}, {password});
+                        MenuHospital({ usuario }, { password });
                     }
                 });
-            } else if (answers.op_menu == false) {
-                MenuHospital({usuario}, {password});
-            }
-        });
+            });
+
+        }
+        else if (answers.op_menu == false) {
+            MenuHospital({ usuario }, { password });
+        }
     });
 }
